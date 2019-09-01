66c2f45bbac991821634e6d07a92591d9dc81443
Eric Hendrix <agentmrbig@gmail.com>
2019-09-01 18:09:45 -0400

    fixed error involving testing, it was in yarn test, in package.json, missing cross-env DB_ENV=testing

---
+++ hobbits/hobbitsModel.test.js
@@ -0,0 +1,22 @@
+const db = require('../data/dbConfig');
+const Hobbits = require('./hobbitsModel');
+
+describe('hobbits', () => {
+    describe('add()', () => {
+        beforeEach(async () => {
+            await db('hobbits').truncate();
+        });
+    });
+
+
+    it('should insert 2 hobbits', async () => {
+        // await Hobbits.add({ name: "dude1" });
+        // await Hobbits.add({ name: "dude2" });
+
+        await db('hobbits').insert({ name: "dude1" });
+        await db('hobbits').insert({ name: "dude2" });
+
+        const hobbits = await db('hobbits');
+        expect(hobbits).toHaveLength(2);
+    });
+});
